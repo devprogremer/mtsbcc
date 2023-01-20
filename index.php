cad27ebe53dc4056e6c2a46bb94e6bff8921c800
devprogremer <devprogremer@gmail.com>
2023-01-20 21:02:04 +0700

    RY

--- index.php
+++ index.php
@@ -137,23 +137,22 @@
 
         <div class="row">
           <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+            <img src="assets/img/About.png" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-            <h3>MTs Bina Cendekia Cirebon</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+          <div class="section-title">
+          <h2>PROGRAM PENDIDIKAN </h2>
+          <p>MTS Bina Cendekia Cirebon </p>
+        </div>
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
+<li><i class="ri-check-double-line"></i> Sebagian besar peserta didik pada jenjang pendidikan dasar dan menengah datang dari keluarga yang kondisi ekonominya adalah menengah ke bawah, dan diantara mereka adalah yatim. Oleh karena itu, mereka hanya diwajibkan membayar uang sumbangan penyelenggaraan pendidikan (SPP) dalam jumlah kecil, dan bagi mereka yang tidak mampu dibebaskan dari kewajiban membayar SPP tersebut.</li>
+<li><i class="ri-check-double-line"></i>Mengikuti program belajar membaca Al Qur’an secara fasih dan belajar tentang kewirausahaan, aqidah, ibadah, and akhlaq adalah wajib bagi setiap peserta didik. </li>
+<li><i class="ri-check-double-line"></i>Program Tahfidz AlQur’an disediakan bagi mereka yang berminat untuk mengikutinya. Dalam program ini peserta dapat memilih program Tahfidz satu Juz’u, Tahfidz lima Juz’u, Tahfidz sepuluh Juz’u, atau Tahfidz seluruhAl Qur’an.</li>
+<li><i class="ri-check-double-line"></i>Disediakan program belajar berkomunikasi menggunakan bahasa Arab, Inggris, Jepang, atau Mandarin yang dapat dipilih oleh peserta didik yang berminat untuk mengikutinya. </li>
+<li><i class="ri-check-double-line"></i>Disediakan program belajar Kitab Kuning (Kutubut Turats)yang disediakan bagi peserta didik yang berminat untuk mengikutinya. </li>
+<li><i class="ri-check-double-line"></i>Disediakan pula proram pemanfaatan masjid kampus (masjid Raudlatut Thalibien) sebagai pusat belajar, khususnya TahfidzAl Qur’an, belajar Kitab Kuning, belajar berkomunikasi menggunakan Bahasa asing, dan kegiatan-kegiatan belajar lainnya. </li>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-            </p>
           </div>
         </div>
 
