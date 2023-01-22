4a66aa2a877755f17fc56a0322ae178d45c40b8f
devprogremer <devprogremer@gmail.com>
2023-01-22 21:14:08 +0700

    ok

--- brosur.php
+++ brosur.php
@@ -1,178 +1,195 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>MTS Bina Cendekia Cirebon</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <!-- Favicons -->
-  <link href="assets/img/faviconmts.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Gp - v4.10.0
-  * Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+    <title>MTS Bina Cendekia</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon-32x32.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/camera.css"/>
+    <link rel="stylesheet" href="css/owl.carousel.css"/>
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    <!--[if (gt IE 9)|!(IE)]><!-->
+    <script src="js/jquery.mobile.customized.min.js"></script>
+    <!--<![endif]-->
+    <script src="js/camera.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <!--[if lt IE 9]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+            <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"
+                 height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+    <![endif]-->
 </head>
-
 <body>
+<div class="page">
+<!--========================================================
+                          HEADER
+=========================================================-->
+<header id="header">
+    <div id="stuck_container">
+        <div class="container">
+            <div class="row">
+                <div class="grid_12">
+                    <div class="brand put-left">
+                        <h1>
+                            <a href="index.html">
+                                <img src="images/logomts.png" alt="Logo"/>
+                            </a>
+                        </h1>
+                    </div>
+                    <nav class="nav put-right">
+                        <ul class="sf-menu">
+                            <li class="current"><a href="index.php">home</a></li>
+                            <li>
+                                <a href="#">Tentang Kami</a>
+                                <ul>
+                                    <li><a href="profil.php">Profil Sekolah</a></li>
+                                    <li><a href="visi.php">Visi Dan Misi</a>
+                                    <li><a href="organisasi.php">Struktur Organisasi</a>
+                                    <li><a href="data.php">Data Guru</a>
+                                    <li><a href="akreditasi.php">Akreditasi</a>
+                                    <li><a href="sarana.php">Sarana</a>
+                                     <ul>
+                                            <li><a href="#">Ruang Kelas</a></li>
+                                            
+                                        </ul>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top ">
-    <div class="container d-flex align-items-center justify-content-lg-between">
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Elearning</a></li>
+                            <li>
+                                <a href="#">Pendaftaran</a>
+                                <ul>
+                                    <li><a href="brosur.php">Brosur</a></li>
+                                    <li><a href="pendaftaran.php">Pendaftaran</a>
+                            </ul>
+                            <li>
+                                <a href="#">Galeri Kegiatan</a>
+                                <ul>
+                                    <li><a href="kegiatan.php">Kegiatan Sekolah</a></li>
+                                    <li><a href="ekstrakulikuler.php">Ekstrakulikuler</a>
+                                                                           
+                                        </ul>
 
-      <h1 class="logo me-auto me-lg-0"> <img src="assets/img/logomts.png" alt="Logo"/><a href="index.php">MTs<span>BCC</span></a></h1>
-
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.php" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
-          <li class="dropdown"><a href="#"><span>Tentang Kami</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="profil.php">Profil Sekolah</a></li>
-              <li><a href="visi.php">Visi Dan Misi</a></li>
-              <li><a href="organisasi.php">Struktur Organisasi</a></li>
-              <li><a href="data.php">Data Guru</a></li>
-              <li><a href="sarana.php">Sarana</a></li>
-            </ul>
-          </li>
-          <li class="dropdown"><a href="#"><span>PPDB</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="brosur.php">Brosur</a></li>
-              <li><a href="formulir.php">Formulir</a></li>
-            </ul>
-          </li>
-          <li class="dropdown"><a href="#"><span>Galery Kegiatan</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="kegiatan.php">Kegiatan Sekolah</a></li>
-              <li><a href="eskul.php">Ekstrakulikuler</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="http://rdm.mtsbinacendekia.sch.id/">Rapot Digital</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-
-      <a href="daftar.php" class="get-started-btn scrollto">Daftar</a>
-
-    </div>
-  </header><!-- End Header -->
-
-  
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>Alamat<span>:</span></h3>
-              <p>
-                Jl. KH Wahid Hasyim <br>
-                Desa. Mertapada Wetan<br> Kecanatan. Astanajapura<br>Kabupaten. Cirebon<br>Jawa Barat 45181<br>
-                <strong>Phone:</strong> 085287222111<br>
-                <strong>Email:</strong> mtsbinacendekia@gmail.com<br>
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="https://www.facebook.com/MTsBinaCendekiaCirebon?mibextid=ZbWKwL" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="https://instagram.com/mtsbinacendekia?igshid=OGQ2MjdiOTE=" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="https://referensi.data.kemdikbud.go.id/tabs.php?npsn=20278791" class="google"><i class="bx bxl-google"></i></a>
-                <a href="https://youtube.com/@mtsbinacendekiacirebon6066" class="youtube"><i class="bx bxl-youtube"></i></a>
-              </div>
+                                    </li>
+                                    <li>
+                                <a href="http://rdm.mtsbinacendekia.sch.id/">Rapot Digital</a>
+</li>
+</ul>
+                           
+                    </nav>
+                </div>
             </div>
-          </div>
-          
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Informasi Yang Bermanfaat</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Bootstrap</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Comment</h4>
-            <p>Informasi lebih lengkap </p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div>
-
         </div>
-      </div>
     </div>
-
+</header>
+<!--========================================================
+                          CONTENT
+=========================================================-->
+<section id="content"><div class="ic">MTS Bina Cendekia 2022</div>
+<div class="camera-wrapper">
+    <div id="camera" class="camera-wrap">
+        <div data-src="images/ppdb5.jpeg" >
+            <div class="fadeIn camera_caption">
+                <!--<h2 class="text_1 color_1">Solutions that you need!</h2>-->
+                <a class="btn_1" href="http://localhost/projectmtsbc-1/ppdb/">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide2.jpeg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Selamat Datang di MTS Bina Cendekia Cirebon</h2>
+                <a class="btn_1" href="#">Daftar</a>
+            </div>
+        </div>
+        <div data-src="images/index_slide3.jpg">
+            <div class="fadeIn camera_caption">
+                <h2 class="text_1 color_1">Sekolah Kita Gudangnya Akhlakul Karimah</h2>
+                <a class="btn_1" href="#">More info</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg_1 wrap_2 wrap_4">
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>MTs</span></strong>BINA CENDEKIA CIREBON
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-        Designed by <a href="">Team MJ</a>
-      </div>
+        <div class="row">
+            <div class="preffix_2 grid_8">
+                <h2 class="header_2 wrap_3 color_2">
+                    <center>Berikut Adalah Informasi PPDB<center></br>
+                        <center> Mts Bina Cendekia Cirebon <center>
+                </h2>
+            </div>
+        </div>
+
+<center><img src="images/ppdb1.jpeg" width="1000" height="700"><center>
+
+<div class="bg_1 wrap_7 wrap_5">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <center><h2 class="header_2 wrap_8 color_2"><center>
+                    Sekolah Kita Gudangnya Akhlakul Karimah
+                </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="grid_12">
+                <div id="owl">
+                    <div class="item">
+                        <p class="text_3">
+Kita siap melayani dan mendidik peserta didik baru dengan benar dan baik dengan ketulusan hati yang paling dalam, kita akan 
+mengajarkan tentang apa arti kehidupan tanpa menjalani perintah Allah dan kita akan mengajarkan pentingnya 5S(Salam, Senyum, Sapa, Sopan dan Santun)
+                          </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </footer><!-- End Footer -->
+</div>
 
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
+<div class="container">
+    <div class="row wrap_9 wrap_4 wrap_10">
+        <div class="grid_12">
+            <div class="header_1 wrap_3 color_3">
+                Get in Touch
+            </div>
+            <div class="box_3">
+                <ul class="list_1">
+                    <li><a class="fa fa-twitter" href="#"></a>@mtsbinacendekiacirebon</li>
+                    <li><a class="fa fa-facebook" href="#"></a>Mts Bina Cendekia Cirebon </li>
+                    <li><a class="fa fa-google-plus" href="#"></a>mtsbcc@gmail.com</li>
+                    <li><a class="fa fa-pinterest" href="#"></a>Mts Bina Cendekia</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</div>
+<!--========================================================
+                          FOOTER
+=========================================================-->
+<footer id="footer" class="color_9">
+    <div class="container">
+        <div class="row">
+            <div class="grid_12">
+                <p class="info text_4 color_4">
+                    © <span id="copyright-year"></span> | <a href="#">Privacy Policy</a> <br/>
+                    Website designed by <a href="http://www.templatemonster.com/" rel="nofollow">TemplateMonster.com</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/script.js"></script>
 </body>
-
 </html>
