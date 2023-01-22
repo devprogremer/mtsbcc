e1279724348628c5e55f293e206c541892474c45
“devprogremer” <devprogremer@gmai.com>
2023-01-22 22:10:08 +0700

    pamol

--- index.php
+++ index.php
@@ -816,7 +816,7 @@ Sebagai konsekuensi Lembaga yang dikelola oleh Dapartemen Agama tidak sama nasib
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/ -->
-        Designed by <a href="">Team MJ</a>
+        Designed by <a href="https://web.facebook.com/profile.php?id=100089766366337">DevProgremer</a>
       </div>
     </div>
   </footer><!-- End Footer -->
