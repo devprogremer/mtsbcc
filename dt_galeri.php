241b8c83df4d09ef6e2bd449495fd449d7172df2
devprogremer <devprogremer@gmail.com>
2023-01-26 16:02:54 +0700

    rianti

--- dt_galeri.php
+++ dt_galeri.php
@@ -8,6 +8,11 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.4.1.js"></script>
 <!-- Favicons -->
 <link href="assets/img/faviconmts.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
@@ -53,55 +58,124 @@
   </div>
 </nav>
     <!-- Optional JavaScript; choose one of the two! -->
-    <center><h1>Data Produk</h1><center>
-    <center><a href="tambah_galery.php">+ &nbsp; Tambah Galery</a><center>
-    <br/>
-    
-      <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">NO</th>
-      <th scope="col">Tanggal</th>
-      <th scope="col">Kegiatan</th>
-      <th scope="col">Gambar</th>
-      <th scope="col">Action</th>
-    </tr>
-   </thead>
-    <tbody>
-      <?php
-      // jalankan query untuk menampilkan semua data diurutkan berdasarkan nim
-      $query = "SELECT * FROM produk ORDER BY id ASC";
-      $result = mysqli_query($koneksi, $query);
-      //mengecek apakah ada error ketika menjalankan query
-      if(!$result){
-        die ("Query Error: ".mysqli_errno($koneksi).
-           " - ".mysqli_error($koneksi));
-      }
+    <br>
+    <div class="container">
+        <?php
+        //Validasi untuk menampilkan pesan pemberitahuan
+        if (isset($_GET['add'])) {
+      
+            if ($_GET['add']=='berhasil'){
+                echo"<div class='alert alert-success'><strong>Berhasil!</strong> File gambar telah diupload!</div>";
+            }else if ($_GET['add']=='gagal'){
+                echo"<div class='alert alert-danger'><strong>Gagal!</strong> File gambar gagal diupload!</div>";
+            }    
+        }
 
-      //buat perulangan untuk element tabel dari data mahasiswa
-      $no = 1; //variabel untuk membuat nomor urut
-      // hasil query akan disimpan dalam variabel $data dalam bentuk array
-      // kemudian dicetak dengan perulangan while
-      while($row = mysqli_fetch_assoc($result))
-      {
-      ?>
-       <tr>
-          <td><?php echo $no; ?></td>
-          <td><?php echo $row['tanggal']; ?></td>
-          <td><?php echo substr($row['kegiatan'], 0, 20); ?>...</td>
-         <td style="text-align: center;"><img src="gambar/<?php echo $row['gambar_produk']; ?>" style="width: 120px;"></td>
-          <td>
-              <a href="edit_produk.php?id=<?php echo $row['id']; ?>">Edit</a> |
-              <a href="proses_hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('Anda yakin akan menghapus data ini?')">Hapus</a>
-          </td>
-      </tr>
-         
-      <?php
-        $no++; //untuk nomor urut terus bertambah 1
-      }
-      ?>
-    </tbody>
-    </table>
+        if (isset($_GET['hapus'])) {
+    
+            if ($_GET['hapus']=='berhasil'){
+                echo"<div class='alert alert-success'><strong>Berhasil!</strong> File gambar telah dihapus!</div>";
+            }else if ($_GET['hapus']=='gagal'){
+                echo"<div class='alert alert-danger'><strong>Gagal!</strong> File gambar gagal dihapus!</div>";
+            }    
+        }
+        ?>
+        <form action="simpan.php" method="post" enctype="multipart/form-data">
+            <!-- rows -->   
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <div id="msg"></div>
+                        <input type="file" name="gambar" class="file" >
+                            <div class="input-group my-3">
+                                <input type="text" class="form-control" disabled placeholder="Upload Gambar" id="file">
+                                <div class="input-group-append">
+                                        <button type="button" id="pilih_gambar" class="browse btn btn-dark">Pilih Gambar</button>
+                                </div>
+                            </div>
+                        <img src="gambar/80x80.png" id="preview" class="img-thumbnail">
+                    </div>
+                </div>
+            </div>
+
+                <button type="submit" name="btn_simpan" class="btn btn-success">Simpan</button>
+        </form>
+
+        <hr>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="table-responsive">
+                    <table class="table table-bordered" width='20%'cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th width='60%'>Gambar</th> 
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php
+                            // include database
+                            include 'koneksi.php';
+                            // perintah sql untuk menampilkan daftar bank yang berelasi dengan tabel kategori bank
+                            $sql="select * from gambar order by id_gambar desc";
+                            $hasil=mysqli_query($koneksi,$sql);
+                            $no=0;
+                            //Menampilkan data dengan perulangan while
+                            while ($data = mysqli_fetch_array($hasil)):
+                            $no++;
+                        ?>
+                        <tr>
+                            <td><?php echo $no; ?></td>
+                            <td><img src="gambar/<?php echo $data['gambar'];?>" class="rounded" width='100%' alt="Cinque Terre"></td>
+                            <td><a href="hapus.php?id_gambar=<?php echo $data['id_gambar'];?>&gambar=<?php echo $data['gambar'];?>" onclick="konfirmasi()" class="btn btn-danger" role="button">Hapus</a></td>
+                        </tr>
+                        <!-- bagian akhir (penutup) while -->
+                        <?php endwhile; ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<style>
+    .file {
+    visibility: hidden;
+    position: absolute;
+    }
+</style>
+
+<script>
+
+    function konfirmasi(){
+        konfirmasi=confirm("Apakah anda yakin ingin menghapus gambar ini?")
+        document.writeln(konfirmasi)
+    }
+
+    $(document).on("click", "#pilih_gambar", function() {
+    var file = $(this).parents().find(".file");
+    file.trigger("click");
+    });
+
+    $('input[type="file"]').change(function(e) {
+    var fileName = e.target.files[0].name;
+    $("#file").val(fileName);
+
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        // get loaded data and render thumbnail.
+        document.getElementById("preview").src = e.target.result;
+    };
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+    });
+</script>
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
